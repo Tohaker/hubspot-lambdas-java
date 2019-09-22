93a346548a5e2ff32d1433ca0aad3208c37f76fc
milesbardon <milesbardon@gmail.com>
2019-09-22 22:05:07 +0100

    Improved code coverage and logging

--- download-customers/src/main/java/net/ltcuk/lambda/customers/FTPDownloader.java
+++ download-customers/src/main/java/net/ltcuk/lambda/customers/FTPDownloader.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.net.PrintCommandListener;
 import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
 import org.apache.commons.net.ftp.FTPReply;
 
 import java.io.*;
@@ -39,7 +40,10 @@ public class FTPDownloader {
                 return false;
             }
             log.info(String.format("Connected to FTP Server. Reply Code %d", reply));
-            return ftpClient.login(username, password);
+
+            boolean loginSuccess = ftpClient.login(username, password);
+            log.debug(String.format("Login Response from FTP Server: %s", ftpClient.getReplyString()));
+            return loginSuccess;
         } catch (IOException ie) {
             log.debug(ie.toString());
             return false;
@@ -48,33 +52,62 @@ public class FTPDownloader {
 
     public boolean closeConnection() {
         if (ftpClient != null) {
+            log.info("Closing FTP Connection...");
             try {
                 ftpClient.disconnect();
+                log.info("Connection to FTP Server has closed.");
                 return true;
             } catch (IOException ie) {
                 ie.printStackTrace();
+                log.debug(String.format("Connection to FTP Server could not be closed: %s", ie.toString()));
                 return false;
             }
         } else {
+            log.debug("FTPClient Object was null.");
             return false;
         }
     }
 
-    public boolean download(String remotePath, String localPath) {
-        if (ftpClient.isConnected()) {
+    public boolean download(String remotePath, String remoteFileName, String localPath, String localFileName) {
+        if (fileExists(remotePath, remoteFileName)) {
+            String fullRemotePath = remotePath + remoteFileName;
+            String fullLocalPath = localPath + localFileName;
             ftpClient.enterLocalPassiveMode();
             try {
                 ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
-                File downloadFile = new File(localPath);
+                File downloadFile = new File(fullLocalPath);
                 OutputStream outputStream = new BufferedOutputStream(new FileOutputStream(downloadFile));
-                boolean success = ftpClient.retrieveFile(remotePath, outputStream);
+                boolean success = ftpClient.retrieveFile(fullRemotePath, outputStream);
                 outputStream.close();
                 return success;
             } catch (IOException ie) {
-                ie.printStackTrace();
+                log.debug(ie.toString());
                 return false;
             }
         } else {
+            log.info(String.format("%S could not be downloaded.", remoteFileName));
+            return false;
+        }
+    }
+
+    private boolean fileExists(String remotePath, String fileName) {
+        if ((ftpClient != null) && (ftpClient.isConnected())) {
+            try {
+                log.info(String.format("Checking if file %s exists...", fileName));
+                FTPFile[] fileList = ftpClient.listFiles(remotePath);
+                for (FTPFile file : fileList) {
+                    if (file.getName().equals(fileName)) {
+                        log.info(String.format("Found file %s", file));
+                        return true;
+                    }
+                }
+                return false;
+            } catch (IOException ie) {
+                log.debug(ie.toString());
+                return false;
+            }
+        } else {
+            log.info("FTPClient not connected.");
             return false;
         }
     }
--- download-customers/src/test/java/net/ltcuk/lambda/customers/FTPDownloaderShould.java
+++ download-customers/src/test/java/net/ltcuk/lambda/customers/FTPDownloaderShould.java
@@ -1,8 +1,10 @@
 package net.ltcuk.lambda.customers;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockftpserver.core.command.CommandNames;
+import org.mockftpserver.core.command.ConnectCommandHandler;
 import org.mockftpserver.fake.FakeFtpServer;
 import org.mockftpserver.fake.UserAccount;
 import org.mockftpserver.fake.filesystem.DirectoryEntry;
@@ -23,8 +25,8 @@ public class FTPDownloaderShould {
             "OVR90120,\"02089929829\",01/11/2018,30/11/2018,1,1.2200,1.2200,\"I2e Calling Line Id Presentation\",,,1.9900,1.9900,\"I2e Calling Line Id Presentation\",OEU00244,OEU00244,\"VAT 20%\",\"VAT 20%\"\n" +
             "OVR90120,\"02089976734\",01/11/2018,30/11/2018,2,2.1000,4.2000,\"I2e Level 4 Service Care\",,,2.4000,4.8000,\"I2e Level 4 Service Care\",OEU00876,OEU00876,\"VAT 20%\",\"VAT 20%\"\n";
 
-    @BeforeAll
-    public static void setup() {
+    @BeforeEach
+    public void setup() {
         fakeFtpServer = new FakeFtpServer();
         fakeFtpServer.addUserAccount(new UserAccount(username, password, "/Monthly"));
 
@@ -37,27 +39,85 @@ public class FTPDownloaderShould {
         fakeFtpServer.start();
     }
 
-    @AfterAll
-    public static void teardown() {
+    @AfterEach
+    public void teardown() {
         fakeFtpServer.stop();
     }
 
     @Test
-    void logIntoAnFTPServer() {
+    void returnTrueUponSuccessfulLogIntoServer() {
         FTPDownloader downloader = new FTPDownloader(username, password, "localhost", fakeFtpServer.getServerControlPort());
         boolean successful = downloader.openConnection();
         downloader.closeConnection();
-        assert(successful);
+        assert (successful);
     }
 
     @Test
-    void downloadFile() {
+    void returnFalseWhenLoginInfoIsIncorrect() {
+        FTPDownloader downloader = new FTPDownloader(username, "wrongpassword", "localhost", fakeFtpServer.getServerControlPort());
+        boolean successful = downloader.openConnection();
+        assert (!successful);
+    }
+
+    @Test
+    void returnFalseWhenAConnectionCantBeOpened() {
+        ConnectCommandHandler cch = new ConnectCommandHandler();
+        cch.setReplyCode(425); // Can't open data connection.
+        fakeFtpServer.setCommandHandler(CommandNames.CONNECT, cch);
+        FTPDownloader downloader = new FTPDownloader(username, password, "localhost", fakeFtpServer.getServerControlPort());
+        boolean successful = downloader.openConnection();
+        assert (!successful);
+    }
+
+    @Test
+    void returnFalseWhenNoServerIsAvailable() {
+        fakeFtpServer.stop(); // Stop the server prior to connecting.
+        FTPDownloader downloader = new FTPDownloader(username, password, "localhost", fakeFtpServer.getServerControlPort());
+        boolean successful = downloader.openConnection();
+        assert (!successful);
+    }
+
+    @Test
+    void returnTrueWhenTheConnectionIsSuccessfullyClosed() {
         FTPDownloader downloader = new FTPDownloader(username, password, "localhost", fakeFtpServer.getServerControlPort());
         downloader.openConnection();
-        boolean successful = downloader.download("/Monthly/2019/file.csv", "file.csv");
+        boolean successful = downloader.closeConnection();
+        assert (successful);
+    }
+
+    @Test
+    void returnFalseWhenTheConnectionWasNotOpened() {
+        FTPDownloader downloader = new FTPDownloader(username, password, "localhost", fakeFtpServer.getServerControlPort());
+        boolean successful = downloader.closeConnection(); // Close the connection before opening it - surefire way to fail the test.
+        assert (!successful);
+    }
+
+    @Test
+    void returnTrueWhenAFileIsSuccessfullyDownloaded() {
+        FTPDownloader downloader = new FTPDownloader(username, password, "localhost", fakeFtpServer.getServerControlPort());
+        downloader.openConnection();
+        boolean successful = downloader.download("/Monthly/2019/", "file.csv", "", "file.csv");
         downloader.closeConnection();
-        new File("file.csv").delete();
-        assert(successful);
+        new File("file.csv").delete(); // Tidy up the local workspace.
+        assert (successful);
+    }
+
+    @Test
+    void returnFalseWhenAFileCannotBeDownloaded() {
+        FTPDownloader downloader = new FTPDownloader(username, password, "localhost", fakeFtpServer.getServerControlPort());
+        downloader.openConnection();
+        boolean successful = downloader.download("/Monthly/2019/", "fakefile.csv", "", "fakefile.csv");
+        downloader.closeConnection();
+        new File("fakefile.csv").delete(); // Tidy up the local workspace.
+        assert (!successful);
+    }
+
+    @Test
+    void returnFalseWhenTryingToDownloadAFileWhenTheConnectionIsClosed() {
+        FTPDownloader downloader = new FTPDownloader(username, password, "localhost", fakeFtpServer.getServerControlPort());
+        boolean successful = downloader.download("/Monthly/2019/", "fakefile.csv", "", "fakefile.csv");
+        new File("fakefile.csv").delete(); // Tidy up the local workspace.
+        assert (!successful);
     }
 
 }
